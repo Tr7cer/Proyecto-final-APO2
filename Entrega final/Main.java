0f6f3392752f742e0f052ce21e41c5b0d255aaa1
David Hernández <107824698+Tr7cer@users.noreply.github.com>
2024-12-04 18:14:01 -0500

    Delete Entrega final/Main.java

--- Entrega final/Main.java
+++
@@ -1,8 +0,0 @@
-package main.usc.musicCollection;
-
-public class Main {
-    public static void main(String[] args) {
-        MainWindow myMainWindow = new MainWindow();
-        myMainWindow.setVisible(true);
-    }
-}
