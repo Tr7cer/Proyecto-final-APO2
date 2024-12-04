9714dde378758e2b5b48fe6438accf39b618dafd
David Hernández <107824698+Tr7cer@users.noreply.github.com>
2024-12-04 18:13:46 -0500

    Delete Entrega final/Labels.java

--- Entrega final/Labels.java
+++
@@ -1,9 +0,0 @@
-package main.usc.musicCollection;
-
-import javax.swing.*;
-
-public class Labels extends JLabel {
-    public Labels(String labelsText) {
-        this.setText(labelsText);
-    }
-}
